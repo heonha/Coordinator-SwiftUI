93ed44c0ae1c7ec392c5a38c5ebe29868bf5fb6b
HeonJin Ha <weizen511@gmail.com>
2023-04-20 21:45:48 +0900

    Initial Commit

---
+++ Coordinator-SwiftUI/Coordinator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_SwiftUIApp.swift
+//  Coordinator-SwiftUI
+//
+//  Created by Heonjin Ha on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
